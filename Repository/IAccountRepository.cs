795ca766ee1d0003ad282014fe23dc8172fa16c3
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2022-12-03 23:15:55 +0700

    Dat: Fix UI and Done Login

--- Repository/IAccountRepository.cs
+++ Repository/IAccountRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessObject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace Repository
 {
     public interface IAccountRepository
     {
+        Task<Account> CheckLogin(string email, string password);
+        Task<Account> GetAccountByEmail(string email);
     }
 }

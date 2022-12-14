2feabfaa71b33588e368606330c8fe4d05e91217
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2022-12-14 10:41:37 +0700

    Dat: fix bug chuyen page cho Bảo

--- ComesticShop/Pages/Shop.cshtml.cs
+++ ComesticShop/Pages/Shop.cshtml.cs
@@ -2,15 +2,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessObject.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Repository;
 
 namespace ComesticShop.Pages
 {
     public class ShopModel : PageModel
     {
-        public void OnGet()
+
+        private readonly ILogger<ShopModel> _logger;
+        private readonly IAccountRepository _accountRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public string Msg { get; set; }
+        public Account Account { get; set; }
+
+        public string Email { get; set; }
+        public string Role { get; set; }
+
+        public IEnumerable<Category> Category1 { get; set; }
+        public IEnumerable<Category> Category2 { get; set; }
+
+        public ShopModel(ILogger<ShopModel> logger, IAccountRepository accountRepository, IProductRepository productRepository, ICategoryRepository categoryRepository)
         {
+            _logger = logger;
+            _accountRepository = accountRepository;
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task OnGet()
+        {
+            Email = HttpContext.Session.GetString("Email");
+            Role = HttpContext.Session.GetString("Role");
+            Account = await _accountRepository.GetAccountByEmail(Email);
+            Category1 = await _categoryRepository.GetCategoriesByType(1);
+            Category2 = await _categoryRepository.GetCategoriesByType(2);
         }
     }
 }
